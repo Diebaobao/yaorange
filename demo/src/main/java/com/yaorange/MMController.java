e529594dedb46d74d3fe03aac6c33cf1627db8f9
127.0.0.1\Administrator <1003015805@qq.com>
2017-05-16 16:19:15 +0800

    zhangsan

--- demo/src/main/java/com/yaorange/MMController.java
+++ demo/src/main/java/com/yaorange/MMController.java
@@ -4,5 +4,6 @@ public class MMController {
 public static void main(String[] args) {
 	System.out.println("3开发的视频通话功能");
 	System.out.println("3开发摇一摇功能");
+	System.out.println("zhangsan genggai");
 }
 }
